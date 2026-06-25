115bfd44baef515ca83c71dc07f5c72c83285c46
Derek Scott <598517+johnny13@users.noreply.github.com>
2026-06-25 10:40:14 -0500

    fix: admin icon fix

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -79,9 +79,9 @@ class ServiceProvider extends BaseServiceProvider
   protected function bootAddonNav(): void
   {
 	  \Statamic\Facades\CP\Nav::extend(function ($nav) {
-          $nav->tools('YouTube Listr') // ⚡ MAGIC METHOD: Groups it properly under Tools
-              ->url(cp_route('statamic-ytlistr.index'))
-              ->icon('video');
+          $nav->tools('YouTube Listr')
+              ->icon('<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" d="m15.75 10.5 4.72-4.72a.75.75 0 0 1 1.28.53v11.38a.75.75 0 0 1-1.28.53l-4.72-4.72M4.5 18.75h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25h9a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25Z" /></svg>')
+              ->url(cp_route('statamic-ytlistr.index'));
       });
   }
 
